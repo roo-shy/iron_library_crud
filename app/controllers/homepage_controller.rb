9cc2d03d2f9158cfd01b8d32c21914871f74179f
Rooshy <ru_sha_nia@yahoo.ca>
2016-03-09 16:48:11 -0600

    Authentication

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,19 @@
 class HomepageController < ApplicationController
   def show
+
+    # if we've visited before, show the number that I store
+    # else store a number, and show it to they
+
+    @number = session[:secret_number]
+    if @number.nil?
+      @number = (1..1000).to_a.sample
+      session[:secret_number] = @number
+    end
+
+    user_id = session[:user_id]
+    if user_id.present?
+      @current_user = User.find_by id: user_id
+    end
+
   end
 end
