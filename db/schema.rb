6dc7cff50ffeb9cdf8e3b3043232067929cdc476
Rooshy <ru_sha_nia@yahoo.ca>
2016-03-23 14:01:50 -0500

    Adding Refile

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160310045511) do
+ActiveRecord::Schema.define(version: 20160323173452) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,8 +38,9 @@ ActiveRecord::Schema.define(version: 20160310045511) do
     t.string   "email"
     t.string   "password"
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+    t.string   "profile_image_id"
   end
 
 end
