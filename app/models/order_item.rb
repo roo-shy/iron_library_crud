6122633f27683e3725a8710775f5c9c4cb3a5a42
Rooshy <ru_sha_nia@yahoo.ca>
2016-03-30 22:29:32 -0500

    Index and View for Orders, Routes, Editing Header

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,8 @@
 class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :book
+
+  def total_price
+    quantity * price
+  end 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160325010812) do
+ActiveRecord::Schema.define(version: 20160330203012) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,11 @@ ActiveRecord::Schema.define(version: 20160325010812) do
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
     t.string   "book_image_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "refile_attachments", force: :cascade do |t|
